27a3ec1f9c29e7fac683aa491698e50e74e05f68
vespacarter <skasez@gmail.com>
2015-10-31 12:06:54 +0100

    piece from desired position found in board

--- board.rb
+++ board.rb
@@ -1,4 +1,5 @@
 class Board
+#	attr_accessor :board, :movements
 	def initialize(loader,board_file,movements_file)
 		@board = Array.new(8) {Array.new(8)}
 		@movements = []
@@ -9,31 +10,88 @@ class Board
 
 	def start
 		load_board(@board_file,@board)
+		#show_board()
 		load_movements(@movements_file,@movements)
+		check_movements()
 	end
 
+
+private
 	def load_board(file,board)
 		@loader.board_loader(file,board)
 	end
 
+	def show_board
+		@board.each_with_index do |row, row_index|
+			row.each_with_index do |col, col_index|
+				cols = col_index.to_i
+				rows = row_index.to_i
+				puts "#{rows},#{cols}: #{@board[rows][cols]}"
+			end
+		end
+	end
+
 	def load_movements(file,movements)
-		@loader.movements_loader(file,movements)
+		@movements = @loader.movements_loader(file)
 	end
 
 	def check_movements
-
+		@movements.each do |movement|
+			move = movement
+			move_array = move.split(" ")
+			start_position = coordenates_converter(move_array[0])
+			end_position = coordenates_converter(move_array[1])
+			piece = get_piece_from_board(start_position)
+		end
 	end
 
-	def coordenates_converter(string)
+	def get_piece_from_board(position)
+		coordenates = position.split("")
+		col = position[0].to_i
+		row = position[1].to_i
+		piece_string = @board[row][col]
+		puts "piece is #{piece_string[1]}"
+		#piece_string
+	end
 
+
+
+
+	def coordenates_converter(string)
+		converted = ""
+		array = string.split("")
+		row = vertical_converter(array[0])
+		col = horizontal_converter(array[1])
+		converted = row + col
+		converted
 	end
 
 	def horizontal_converter(string)
-
+		col = {
+			"8" => "0",
+			"7" => "1",
+			"6" => "2",
+			"5" => "3",
+			"4" => "4",
+			"3" => "5",
+			"2" => "6",
+			"1" => "7",
+		}
+		col[string]
 	end
 
-	def vertical_converter(string)
-
+	def vertical_converter(string)	
+		row = {
+			"a" => "0",
+			"b" => "1",
+			"c" => "2",
+			"d" => "3",
+			"e" => "4",
+			"f" => "5",
+			"g" => "6",
+			"h" => "7",
+		}
+		row[string]
 	end
 
 
--- loader.rb
+++ loader.rb
@@ -2,18 +2,20 @@ class Loader
 
 	def board_loader(file,board)
 		board_string = IO.read(file)
-		lines_array = board_string.split("\r\n")
+		lines_array = board_string.split("\n")
 		lines_array.each_with_index do |row, row_index| 
 			row_array = row.split(" ")
-			row_array.each_with_index do |item, col_index| 	
+			row_array.each_with_index do |item, col_index|
+			#	puts "row #{row_index}, col #{col_index}, value #{item}" 	
 				board[row_index][col_index] = item
 			end
 		end
 	end
 
-	def movements_loader(file,movements)
+	def movements_loader(file)
 		movements_string = IO.read(file)
 		movements = movements_string.split("\n")
+		movements
 	end
 
 end
